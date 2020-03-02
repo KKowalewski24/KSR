91a8124308b8bde3c52b52a91910b8a4ea530a98
Kamil Kowalewski <szamil128@onet.pl>
2020-03-02 20:28:47 +0100

    Add Document

---
+++ Task1/src/main/java/pl/jkkk/task1/model/Document.java
@@ -0,0 +1,17 @@
+package pl.jkkk.task1.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Document {
+
+    /*------------------------ FIELDS REGION ------------------------*/
+
+    /*------------------------ METHODS REGION ------------------------*/
+
+}
+    

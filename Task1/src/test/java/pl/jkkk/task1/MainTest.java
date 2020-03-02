19ee6fabaed9fc5c3a2df671358096e786a335f7
Kamil Kowalewski <szamil128@onet.pl>
2020-03-02 20:18:37 +0100

    Add basic project for task1

---
+++ Task1/src/test/java/pl/jkkk/task1/MainTest.java
@@ -0,0 +1,13 @@
+package pl.jkkk.task1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void contextLoads() {
+        assertEquals(5, 5);
+    }
+}

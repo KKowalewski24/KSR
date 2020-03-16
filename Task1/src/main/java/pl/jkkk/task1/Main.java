85b96db0db6b334a154a6d6df0389415aa20bd52
Jan Karwowski <karwojan@tlen.pl>
2020-03-16 20:37:13 +0100

    Add keywords extraction

--- Task1/src/main/java/pl/jkkk/task1/Main.java
+++ Task1/src/main/java/pl/jkkk/task1/Main.java
@@ -1,14 +1,13 @@
 package pl.jkkk.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.jkkk.task1.featureextraction.KeywordsExtractor;
 import pl.jkkk.task1.model.Document;
 import pl.jkkk.task1.reader.SgmlFileReader;
 import pl.jkkk.task1.stemmer.DocumentStemmer;
 import pl.jkkk.task1.stopwords.WordRemover;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
-
 import static pl.jkkk.task1.constant.Constants.FILENAME_LIST;
 
 public class Main {
@@ -17,13 +16,42 @@ public class Main {
     private static SgmlFileReader sgmlFileReader = new SgmlFileReader();
     private static DocumentStemmer documentStemmer = new DocumentStemmer();
     private static WordRemover wordRemover = new WordRemover();
+    private static KeywordsExtractor keywordsExtractor;
     private static List<Document> documents;
 
     /*------------------------ METHODS REGION ------------------------*/
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args){
 
-        documents = sgmlFileReader.readFromFiles(FILENAME_LIST);
-        documentStemmer.performStemmingProcessOnWordList(documents);
-        wordRemover.removeStopWords(documents);
+        action(() -> {
+            try{
+                documents = sgmlFileReader.readFromFiles(FILENAME_LIST);
+            }catch(Exception e){
+                throw new RuntimeException(e);
+            }}, "Reading documents");
+
+        action(() -> documentStemmer.performStemmingProcessOnWordList(documents),
+                "Stemming documents");
+
+        action(() -> {
+            try{
+                wordRemover.removeStopWords(documents);
+            }catch(Exception e){
+                throw new RuntimeException(e);
+            }}, "Removing stop words");
+
+        keywordsExtractor = new KeywordsExtractor(documents);
+        action(() -> keywordsExtractor.calculateOccurrences(),
+                "Calculating word occurrences");
+
+        final List<String> keywords = new ArrayList<>();
+        action(() -> keywords.addAll(keywordsExtractor.getKeywordsByTPSD(100)),
+                "Retrieving keywords");
+    }
+
+    private static void action(Runnable runnable, String description){
+        long startTime = System.currentTimeMillis();
+        System.out.print(description + "... ");
+        runnable.run();
+        System.out.println(((System.currentTimeMillis() - startTime) / 1000.0) + "s");
     }
 }
---
+++ Task1/src/main/java/pl/jkkk/task1/featureextraction/KeywordsExtractor.java
@@ -0,0 +1,116 @@
+package pl.jkkk.task1.featureextraction;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import pl.jkkk.task1.model.Document;
+
+public class KeywordsExtractor {
+
+    /* Occurrences per documents for each word */
+    private Map<String, Map<Document, Integer>> occurrences;
+
+    /* Score (from some algorithm) for each word */
+    private Map<String, Double> scores;
+
+    /* All processed documents */
+    private List<Document> documents;
+
+    public KeywordsExtractor(final List<Document> documents){
+        this.occurrences = new HashMap<>();
+        this.scores = new HashMap<>();
+        this.documents = documents;
+    }
+
+    /** 
+     * Calculate occurrences per document for each word from documents' list,
+     * must be called before any keywords retrieving method.
+     *
+     * @param documents list of documents
+     */
+    public void calculateOccurrences(){
+        if(documents.isEmpty())
+            throw new NoDocumentsException();
+        occurrences.clear();
+        documents.forEach(document -> {
+            document.getWordList().forEach(word -> {
+                if(!occurrences.containsKey(word)){
+                    occurrences.put(word, new HashMap<>());
+                }
+                Map<Document, Integer> occurrence = occurrences.get(word);
+                occurrence.put(document, 
+                            Optional.ofNullable(occurrence.get(document)).orElse(0) + 1);
+            });
+        });
+    }
+
+    private void calculateScoresUsingTFIDF(){
+        if(occurrences.isEmpty())
+            throw new NoOccurrencesException();
+        scores.clear();
+        occurrences.keySet().forEach(word -> {
+            Map<Document, Integer> occurrence = occurrences.get(word);
+            occurrence.keySet().forEach(document -> {
+                double tf = occurrence.get(document) / 
+                    (double) document.getWordList().size();
+                double idf = documents.size() / 
+                    (double) occurrence.keySet().size();
+                scores.put(word, 
+                        Optional.ofNullable(scores.get(word)).orElse(0.0) + tf * idf);
+            });
+        });
+    }
+
+    /**
+     * Calculate scores using Terms Per Squared Documents' number method.
+     */
+    private void calculateScoresUsingTPSD(){
+        if(occurrences.isEmpty())
+            throw new NoOccurrencesException();
+        scores.clear();
+        occurrences.keySet().forEach(word -> {
+            double sum = occurrences.get(word).values().stream()
+                .mapToDouble(value -> value).sum();
+            double count = occurrences.get(word).values().size();
+            scores.put(word, sum / (count * count));
+        });
+    }
+
+    private List<String> retrieveKeywords(int numberOfKeywords){
+        return scores.keySet().stream()
+            .sorted((String word1, String word2) -> {
+               if(scores.get(word1) < scores.get(word2)) return -1;
+                else if(scores.get(word1) > scores.get(word2)) return 1;
+                else return 0;
+            })
+            .limit(numberOfKeywords)
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Create list of keywords using modified TF-IDF algorithm.
+     *
+     * @param numberOfKeywords number of keywords to retrieve
+     */
+    public List<String> getKeywordsByTFIDF(int numberOfKeywords){
+        calculateScoresUsingTFIDF();
+        return retrieveKeywords(numberOfKeywords);
+    }
+
+    /**
+     * Create list of keywords using Terms Per Squared Document's number algorithm.
+     *
+     * @param numberOfKeywords number of keywords to retrieve
+     */
+    public List<String> getKeywordsByTPSD(int numberOfKeywords){
+        calculateScoresUsingTPSD();
+        return retrieveKeywords(numberOfKeywords);
+    }
+
+    public class NoOccurrencesException extends RuntimeException{}
+
+    public class NoDocumentsException extends RuntimeException{}
+}

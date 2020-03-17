c42968099c9d77e2f6a9d8b65c805360883aeaa0
Jan Karwowski <karwojan@tlen.pl>
2020-03-17 18:11:44 +0100

    Optimize stop words remover

--- Task1/src/main/java/pl/jkkk/task1/stopwords/WordRemover.java
+++ Task1/src/main/java/pl/jkkk/task1/stopwords/WordRemover.java
@@ -1,24 +1,24 @@
 package pl.jkkk.task1.stopwords;
 
+import java.net.URISyntaxException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import pl.jkkk.task1.exception.ReaderException;
 import pl.jkkk.task1.model.Document;
 import pl.jkkk.task1.reader.JsonFileReader;
-
-import java.net.URISyntaxException;
-import java.util.List;
-
 import static pl.jkkk.task1.constant.Constants.STOP_WORDS;
 
 public class WordRemover {
 
     /*------------------------ FIELDS REGION ------------------------*/
     private JsonFileReader jsonFileReader = new JsonFileReader();
-    private List<String> stopWords;
 
     /*------------------------ METHODS REGION ------------------------*/
     public void removeStopWords(List<Document> documents)
             throws URISyntaxException, ReaderException {
-        stopWords = jsonFileReader.readFromFile(STOP_WORDS);
+        Set<String> stopWords = new HashSet<>(jsonFileReader.readFromFile(STOP_WORDS));
 
         for (Document it : documents) {
             it.getWordList().removeIf((jt) -> stopWords.contains(jt));

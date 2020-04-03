eaab64a78f548a9544f178af5114e8a7b73cb7c6
Kamil Kowalewski <szamil128@onet.pl>
2020-04-03 10:20:11 +0200

    Adapt python script

--- Automatization/main.py
+++ Automatization/main.py
@@ -5,38 +5,40 @@ How to use
 Copy generated jar with dependencies to directory 'Automatization', set jar file name,
 optionally add parameters and run in cmd by typing python main.py
 
+ "Required parameters:  \n" +
+                        "\t<percentage of training set (integer 1-99)>\n" +
+                        "\t<k for kNN (integer >0)>\n" +
+                        "\t<number of keywords (integer >0)>\n" +
+                        "\t<numerical metric [eucl|manh|cheb]>\n" +
+                        "\t<text metric [trigram|tfm]>"
+
 Sample call of the function 
+
 subprocess.call(
-        ["java", "-jar", JAR_NAME, "features", "60", "5", "5", "eucl"], shell=True
+        ["java", "-jar", JAR_NAME, training_set, knn, keywords, metric, text_metric]
     )
+    
+call_jar_with_five_args("", "", "", "", "")
     
 '''
 
 JAR_NAME = "task1-0.0.1-jar-with-dependencies.jar"
 
 
-# For ngram
-def call_jar_with_four_args(classification, training_set, knn, number_n):
+def call_jar_with_five_args(training_set, knn, keywords, metric, text_metric):
     subprocess.call(
-        ["java", "-jar", JAR_NAME, classification, training_set, knn, number_n]
-    )
-
-
-# For features and tfm
-def call_jar_with_five_args(classification, training_set, knn, keywords, metric):
-    subprocess.call(
-        ["java", "-jar", JAR_NAME, classification, training_set, knn, keywords, metric]
+        ["java", "-jar", JAR_NAME, training_set, knn, keywords, metric, text_metric]
     )
 
 
 def main():
     for i in range(3):
         print("Iteration number(counted from 1): " + str(i + 1))
-        call_jar_with_five_args("features", "60", "5", "5", "eucl")
+        call_jar_with_five_args("", "", "", "", "")
 
-    print("==============")
+    print("==========================================================================")
     print("END OF PROCESS")
-    print("==============")
+    print("==========================================================================")
 
 
 if __name__ == "__main__":

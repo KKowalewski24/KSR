1d7cc12037a095ffe4d92cc856b2f548c529ac17
Kamil Kowalewski <szamil128@onet.pl>
2020-04-04 09:26:51 +0200

    Extend script

--- Automatization/main.py
+++ Automatization/main.py
@@ -32,6 +32,7 @@ def call_jar_with_five_args(training_set, knn, keywords, metric, text_metric):
     )
 
 
+# ----------------------------------------------------------------------------- #
 def series(training_set, k, number_of_keywords):
     call_jar_with_five_args(training_set, k, number_of_keywords, "eucl", "tfm")
     call_jar_with_five_args(training_set, k, number_of_keywords, "manh", "tfm")
@@ -39,6 +40,20 @@ def series(training_set, k, number_of_keywords):
     call_jar_with_five_args(training_set, k, number_of_keywords, "eucl", "trigram")
     call_jar_with_five_args(training_set, k, number_of_keywords, "manh", "trigram")
     call_jar_with_five_args(training_set, k, number_of_keywords, "cheb", "trigram")
+
+
+def first_round():
+    series("40", "3", "1000")
+    series("40", "5", "1000")
+    series("40", "9", "1000")
+    series("40", "19", "1000")
+    series("40", "51", "1000")
+
+    series("40", "3", "10000")
+    series("40", "5", "10000")
+    series("40", "9", "10000")
+    series("40", "19", "10000")
+    series("40", "51", "10000")
 
 
 # ----------------------------------------------------------------------------- #
@@ -66,44 +81,39 @@ def second_round():
     series_1("17")
     series_1("19")
 
-    # series_2("1")
-    # series_2("3")
-    # series_2("5")
-    # series_2("7")
-    # series_2("9")
-    # series_2("11")
-    # series_2("13")
-    # series_2("15")
-    # series_2("17")
-    # series_2("19")
-    #
-    # series_3("1")
-    # series_3("3")
-    # series_3("5")
-    # series_3("7")
-    # series_3("9")
-    # series_3("11")
-    # series_3("13")
-    # series_3("15")
-    # series_3("17")
-    # series_3("19")
+    series_2("1")
+    series_2("3")
+    series_2("5")
+    series_2("7")
+    series_2("9")
+    series_2("11")
+    series_2("13")
+    series_2("15")
+    series_2("17")
+    series_2("19")
+
+    series_3("1")
+    series_3("3")
+    series_3("5")
+    series_3("7")
+    series_3("9")
+    series_3("11")
+    series_3("13")
+    series_3("15")
+    series_3("17")
+    series_3("19")
+
+
+# ----------------------------------------------------------------------------- #
+def third_round():
+    pass
 
 
 # ----------------------------------------------------------------------------- #
 def main():
-    # series("40", "3", "1000")
-    # series("40", "5", "1000")
-    # series("40", "9", "1000")
-    # series("40", "19", "1000")
-    # series("40", "51", "1000")
-    #
-    # series("40", "3", "10000")
-    # series("40", "5", "10000")
-    # series("40", "9", "10000")
-    # series("40", "19", "10000")
-    # series("40", "51", "10000")
-
-    second_round()
+    # first_round()
+    # second_round()
+    third_round()
 
     print("==========================================================================")
     print("END OF PROCESS")

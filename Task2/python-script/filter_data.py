485748464a3b27e297e662d582296028e1deb910
Jan Karwowski <jan.karwowski@makolab.com>
2020-05-04 08:18:43 +0200

    Change filtering original data

--- Task2/python-script/filter_data.py
+++ Task2/python-script/filter_data.py
@@ -6,9 +6,10 @@ FILTERED_FILE_PATH = "../src/main/resources/filtered_pollution_data.csv"
 
 
 def main():
-    data = pandas.read_csv(INITIAL_FILE_PATH, nrows=100000)
+    data = pandas.read_csv(INITIAL_FILE_PATH)
     data.dropna(how="any", inplace=True)
-    data.drop(data.columns[0], axis=1, inplace=True)
+    data.drop([data.columns[0], data.columns[1], data.columns[2], data.columns[3], 
+        data.columns[9], data.columns[14], data.columns[19], data.columns[24]], axis=1, inplace=True)
     data.to_csv(FILTERED_FILE_PATH, index=False, header=False)
     print(data)
 

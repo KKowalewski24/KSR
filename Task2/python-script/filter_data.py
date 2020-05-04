099ebdb9f68f7f6adf59c4a2c94491726a034958
Kamil Kowalewski <szamil128@onet.pl>
2020-05-04 09:01:02 +0200

    Improve filter_data.py + add data

--- Task2/python-script/filter_data.py
+++ Task2/python-script/filter_data.py
@@ -1,17 +1,41 @@
+import sys
 import pandas
 
-# Change path for initial file to correct one
-INITIAL_FILE_PATH = "initial_pollution_data.csv"
+# VAR ------------------------------------------------------------------------ #
 FILTERED_FILE_PATH = "../src/main/resources/filtered_pollution_data.csv"
 
 
-def main():
-    data = pandas.read_csv(INITIAL_FILE_PATH)
+# ----------------------------------------------------------------------------- #
+def print_statistics():
+    pass
+
+
+def filter_and_remove(initial_file_path):
+    data = pandas.read_csv(initial_file_path)
     data.dropna(how="any", inplace=True)
-    data.drop([data.columns[0], data.columns[1], data.columns[2], data.columns[3], 
-        data.columns[9], data.columns[14], data.columns[19], data.columns[24]], axis=1, inplace=True)
+    data.drop([data.columns[0], data.columns[1], data.columns[2], data.columns[3],
+               data.columns[9], data.columns[14], data.columns[19], data.columns[24]],
+              axis=1, inplace=True)
     data.to_csv(FILTERED_FILE_PATH, index=False, header=False)
     print(data)
+
+
+def print_finish():
+    print("------------------------------------------------------------------------")
+    print("FINISHED")
+    print("------------------------------------------------------------------------")
+    pass
+
+
+# ----------------------------------------------------------------------------- #
+def main():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "stat":
+            print_statistics()
+        else:
+            filter_and_remove(sys.argv[1])
+    print_finish()
+    pass
 
 
 if __name__ == '__main__':

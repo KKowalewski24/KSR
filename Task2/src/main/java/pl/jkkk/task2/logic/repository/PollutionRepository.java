991cfb5f49fcd32b6148d52b01d1f2ee67684b23
Jan Karwowski <karwojan@tlen.pl>
2020-05-16 11:16:36 +0200

    Remove everything except fuzzy

--- Task2/src/main/java/pl/jkkk/task2/logic/repository/PollutionRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.jkkk.task2.logic.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.jkkk.task2.logic.model.Pollution;
-
-public interface PollutionRepository extends CrudRepository<Pollution, Long> {
-
-}
-    
